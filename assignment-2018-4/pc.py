70852e168b33dc5e9e76a17eb93f36e937066fb3
xaritinikovaiou <xkoveou@yahoo.com>
2018-10-05 03:42:15 +0300

    Input Parse

--- assignment-2018-4/pc.py
+++ assignment-2018-4/pc.py
@@ -1,1 +1,41 @@
-first commit
+inputFileName = sys.argv[1]
+
+with open(inputFileName) as inputFile:
+    with open('results.txt', 'a+') as outputFile:
+        line = inputFile.readline()
+
+        edgesList = list()
+
+        while line:
+
+            newEdgeInfo = line.split()
+
+            newEdgeBeginning = int(newEdgeInfo[0])
+
+            newEdgeEnd = int(newEdgeInfo[1])
+
+            newEdgesList = [newEdgeBeginning, newEdgeEnd]
+
+            edgesList.append(newEdgesList)
+
+            line = inputFile.readline()
+
+        inputGraph = dict()
+
+        bipartiteFromGraphSet = list()
+
+        bipartiteToGraphSet = list()
+
+        bipartiteEdgesList = list()
+
+        for [key, value] in edgesList:
+
+            if not(key in inputGraph.keys()):
+
+                inputGraph[key] = list()
+
+            inputGraph[key].append(value)
+
+            if not(value in inputGraph.keys()):
+
+                inputGraph[value] = list()
